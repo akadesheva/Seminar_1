a973e42ee827e28a0afa91826c08fa1c9f06b0c6
Akmaral.K <kadesheva2023@gmail.com>
2023-04-05 12:18:29 +0500

    added solutions for Home Task 2

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 2
+//Напишите программу, которая навход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Write("a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b){
+    Console.WriteLine("max is a, min is b");
+}else {
+    Console.WriteLine("max is b, min is a");
+}
+
