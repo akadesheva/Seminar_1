dd48a118a8b442ea619b6e15a9b9ca4f706313dc
Akmaral.K <kadesheva2023@gmail.com>
2023-04-05 12:36:03 +0500

    added solution for Home Task 4

---
+++ 4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4
+//Напишите программу, которая принимает на вход три числа и выдает масимальное из этих чисел.
+
+Console.Write("a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("c: ");
+int c = int.Parse(Console.ReadLine());
+if (a > b && a > c) {
+    Console.WriteLine("max is a");
+} 
+else 
+if (b > a && b > c) {
+    Console.WriteLine("max is b");
+} 
+else {
+    Console.WriteLine("max is c");
+}
